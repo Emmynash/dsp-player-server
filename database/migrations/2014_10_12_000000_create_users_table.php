b72ea96576e91f322a29f00831e442115a990888
kator James <kator@analogrepublic.com>
2020-09-11 17:24:23 +0100

    Add dsa_id in users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('dsa_id')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('dsa_token')->nullable();
