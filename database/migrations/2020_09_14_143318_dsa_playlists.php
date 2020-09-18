093a77f15f9ce52b5b542708c706cb999ff9a2ec
kator James <kator@analogrepublic.com>
2020-09-18 07:45:10 +0100

    Add dsa playlist migration

--- database/migrations/2020_09_14_143318_dsa_playlists.php
+++ database/migrations/2020_09_14_143318_dsa_playlists.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDsaPlaylistsTable extends Migration
+class DsaPlaylists extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateDsaPlaylistsTable extends Migration
      */
     public function up()
     {
-        Schema::create('playlists', function (Blueprint $table) {
+        Schema::create('dsa_playlists', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('image_url')->nullable();
-            $table->string('spotify_id')->nullable();
-            $table->string('apple_id')->nullable();
+            $table->json('image_url')->nullable();
+            $table->string('provider')->nullable();
+            $table->string('dsa_playlist_id')->nullable();
             $table->string('name');
             $table->string('description')->nullable();
             $table->timestamps();
@@ -32,6 +32,6 @@ class CreateDsaPlaylistsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('playlists');
+        Schema::dropIfExists('dsa_playlists');
     }
 }
