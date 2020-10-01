ae4284899c78f1fec868f58e3bb44b3ef6b9be14
kator James <kator@analogrepublic.com>
2020-10-01 16:21:24 +0100

    Modified exception message

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,8 @@
 
 namespace App\Exceptions;
 
+use HttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -50,6 +52,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ModelNotFoundException && $request->wantsJson()) {
+            return response()->json(['message' => 'Not Found!'], 404);
+        }
+        if ($exception instanceof HttpException && $request->wantsJson()) {
+            return response()->json(['message' => 'Bad request!'], 400);
+        }
         return parent::render($request, $exception);
     }
 }
